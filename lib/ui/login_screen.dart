61cb8dfe3d20cee7a8a6f469056dc1a1afa88f14
Sikandar Ali <sikandar.6a@gmail.com>
2024-05-30 15:11:49 +0500

    login

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -4,29 +4,11 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-//  Instead of creating Two Screens, I have Added both Login and Signup Screen in one Screen
-//  Yes , I am Lazy , But I am not going to create two screens , I am going to create one screen
-
-//  So for to monitor we are in which State we are i.e Login or signUp , I have used enums here
-//  So I have created and Enum Status which contains two things Login and SignUp
-
-//  and I have made a Global Variable type of Status, to use in LoginPage
-// It's actually not recommended to use Global Variables , but I am using it here to make it simple
-//  The main motive here was to teach Firebase Authentication using Riverpod as state management
-
 enum Status {
   login,
   signUp,
 }
-
 Status type = Status.login;
-
-//  I have used stateful widget to use setstate functions in LoginPage
-//  we could also managed the state using Riverpod but I am not using it here
-//  Remember Stateful widgets are made for a reason. If it would be bad
-//  flutter developer would not think of it in the first place.
-
 class LoginPage extends StatefulWidget {
   static const routename = '/LoginPage';
   const LoginPage({super.key});
@@ -36,16 +18,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  //  GlobalKey is used to validate the Form
   final GlobalKey<FormState> _formKey = GlobalKey();
-
-  //  TextEditingController to get the data from the TextFields
-  //  we can also use Riverpod to manage the state of the TextFields
-  //  but again I am not using it here
   final _email = TextEditingController();
   final _password = TextEditingController();
-
-  //  A loading variable to show the loading animation when you a function is ongoing
   bool _isLoading = false;
   bool _isLoading2 = false;
   void loading() {
@@ -70,7 +45,6 @@ class _LoginPageState extends State<LoginPage> {
         type = Status.signUp;
       });
     }
-    // print(type);
   }
 
   @override
@@ -79,23 +53,11 @@ class _LoginPageState extends State<LoginPage> {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Consumer(builder: (context, ref, _) {
-          //  Consuming a provider using watch method and storing it in a variable
-          //  Now we will use this variable to access all the functions of the
-          //  authentication
           final auth = ref.watch(authenticationProvider);
-
-          //  Instead of creating a clutter on the onPressed Function
-          //  I have decided to create a seperate function and pass them into the
-          //  respective parameters.
-          //  if you want you can write the exact code in the onPressed function
-          //  it all depends on personal preference and code readability
           Future<void> onPressedFunction() async {
             if (!_formKey.currentState!.validate()) {
               return;
             }
-            // print(_email.text); // This are your best friend for debugging things
-            //  not to mention the debugging tools
-            // print(_password.text);
             if (type == Status.login) {
               loading();
               await auth
@@ -121,11 +83,6 @@ class _LoginPageState extends State<LoginPage> {
                     }
                   }));
             }
-
-            //  I had said that we would be using a Loading spinner when
-            //  some functions are being performed. we need to check if some
-            //  error occured then we need to stop loading spinner so we can retry
-            //  Authenticating
           }
 
           Future<void> loginWithGoogle() async {
